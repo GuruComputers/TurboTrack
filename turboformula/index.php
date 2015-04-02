cb1f7678fbac9151d191cecdc0f3b2058bf7b674
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-04-02 23:07:17 +0100

    Added Panel Node.JS Skeleton

--- turboformula/index.php
+++ turboformula/index.php
@@ -51,11 +51,68 @@
           </div>
 
           <div class="inner cover">
-            <h1 class="cover-heading">Coming Soon!</h1>
-            <p class="lead">We are hard at work bringing you a new clan website, stay tuned!</p>
-            <p class="lead">
-              <a href="#" class="btn btn-lg btn-default">Learn more</a>
-            </p>
+            <link class="cssdeck" rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.1/css/bootstrap.min.css">
+            <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.1/css/bootstrap-responsive.min.css" class="cssdeck">
+            <div class="" id="loginModal">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                <h3>Have an Account?</h3>
+              </div>
+              <div class="modal-body">
+                <div class="well">
+                  <ul class="nav nav-tabs">
+                    <li class="active"><a href="#login" data-toggle="tab">Login</a></li>
+                    <li><a href="#create" data-toggle="tab">Apply</a></li>
+                  </ul>
+                  <div id="myTabContent" class="tab-content">
+                    <div class="tab-pane active in" id="login">
+                      <form class="form-horizontal" action='login.php' method="POST">
+                        <fieldset>
+                          <div id="legend">
+                            <legend class="">Login</legend>
+                          </div>    
+                          <div class="control-group">
+                            <!-- Username -->
+                            <label class="control-label"  for="username">Username</label>
+                            <div class="controls">
+                              <input type="text" id="username" name="username" placeholder="" class="input-xlarge">
+                            </div>
+                          </div>
+                          <div class="control-group">
+                            <!-- Password-->
+                            <label class="control-label" for="password">Password</label>
+                            <div class="controls">
+                              <input type="password" id="password" name="password" placeholder="" class="input-xlarge">
+                            </div>
+                          </div>
+                          <div class="control-group">
+                            <!-- Button -->
+                            <div class="controls">
+                              <button class="btn btn-success">Login</button>
+                            </div>
+                          </div>
+                        </fieldset>
+                      </form>                
+                    </div>
+                  <div class="tab-pane fade" id="create">
+                    <form id="tab">
+                      <label>Name</label>
+                      <input type="text" value="" class="input-xlarge">
+                      <label>Steam ID</label>
+                      <input type="text" value="" class="input-xlarge">
+                      <label>Why do you want to join?</label>
+                      <textarea value="Smith" rows="2" class="input-xlarge">
+                      </textarea>
+                      <div>
+                        <button class="btn btn-primary">Create Account</button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <script class="cssdeck" src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+            <script class="cssdeck" src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
           </div>
 
           <div class="mastfoot">
---
+++ turboformula/panel/app.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var stormpath = require('express-stormpath');
+
+var app = express();
+
+app.set('views', './iews');
