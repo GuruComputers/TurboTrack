025a746950df74dde3ada82775f8355e5c4c91fe
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-04-02 17:37:50 +0100

    Add Schema for Trucker Stats

--- noderest/models/trucker.js
+++ noderest/models/trucker.js
@@ -4,7 +4,23 @@ Schema = mongoose.Schema;
 var TruckerSchema = new Schema({
 	id: String,
 	name: String,
-	steamid: String
+	steamid64: String,
+	telemetryPluginVersion: Number,
+	gameVersion: Number,
+	fuelAverageConsumption: String,
+	hasJob: Boolean,
+	jobRemainingTime: Date,
+	sourceCity: String,
+	destinationCity: String,
+	wearEngine: Number,
+	wearTransmission: Number,
+	wearCabin: Number,
+	wearChassis: Number,
+	wearWheels: Number,
+	wearTrailer: Number,
+	truckOdometer: Number,
+	trailerMass: Number,
+	trailerName: String
 });
 
 mongoose.model('Trucker', TruckerSchema);
