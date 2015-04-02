43696ac4120ef04309fe942830751ca5ed4fc285
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-04-02 18:46:28 +0100

    Add Console Logging

--- noderest/controllers/truckers.js
+++ noderest/controllers/truckers.js
@@ -16,6 +16,7 @@ exports.findById = function(req, res) {
 
 exports.add = function(req, res) {
 	Trucker.create(req.body, function (err, trucker) {
+		console.log(req.body);
 		if (err) return console.log(err);
 		return res.send(trucker);
 	});
