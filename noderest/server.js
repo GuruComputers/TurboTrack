ed348de67c0be6224cebd22c337b69e3a54cb344
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-04-02 15:33:24 +0100

    Fix Command Line Prompt

--- noderest/server.js
+++ noderest/server.js
@@ -3,4 +3,3 @@ http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.end('Hello World!\n');
 }).listen(1337);
-console.log('Server running at http://hitec4all.co.uk/turboformula:1337');
