dfc2476ef36b401ab2e251d6205e175c2dc411a4
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-04-15 14:18:50 +0100

    Add Wiki Menu Item

--- index.php
+++ index.php
@@ -27,6 +27,7 @@
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
             <li class="active"><a href="#">Home</a></li>
+            <li><a href="http://wiki.turboformula.co.uk" target="_blank">Wiki</a></li>
             <li><a href="#contact">Contact Us</a></li>
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Projects <span class="caret"></span></a>
